f93d1cb2fa68b9c3d38912af902279d142647bba
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-09 19:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
-    //Write your code here
 
-
+  let clr= document.getElementsByTagName("strong")
+	for(let i=0;i<clr.length;++i){
+		clr[i].style.color="green"
+	}
 }
 
 
 function return_normal() {
     //Write your code here
+	let cl= document.getElementsByTagName("strong")
+	for(let i=0;i<cl.length;++i){
+		cl[i].style.color="black"
+	}
 
     
 }
